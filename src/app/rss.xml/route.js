4775e8c99cf46cdb1deee594a78595cdf8ba8173
James Acres <jamesacres@gmail.com>
2024-01-27 15:03:30 +0000

    implement rss feed

---
+++ src/app/rss.xml/route.js
@@ -0,0 +1,24 @@
+import { getBlogPostList } from '@/helpers/file-helpers';
+import { NextResponse } from 'next/server';
+import RSS from 'rss';
+
+export async function GET(request) {
+  const url = new URL(request.url);
+  const feed = new RSS({
+    feed_url: url.toString(),
+    site_url: `${url.protocol}//${url.host}`,
+    title: 'Project Blog',
+  });
+  const blogPosts = await getBlogPostList();
+  blogPosts.map(({ slug, title, abstract, publishedOn }) =>
+    feed.item({
+      title,
+      date: new Date(publishedOn),
+      description: abstract,
+      url: `${url.protocol}//${url.host}/${slug}`,
+    })
+  );
+  return new NextResponse(feed.xml({ indent: true }), {
+    headers: { 'Content-Type': 'text/xml' },
+  });
+}
